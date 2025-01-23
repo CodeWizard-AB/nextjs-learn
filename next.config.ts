9ecf5ddfa6c312c8dffcb70a68a76874d85d7a58
Anunay Argha <anunayargha@gmail.com>
2025-01-23 21:13:09 +0600

    webpack configure

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
-	webpack: (config) => {
-		config.externals = {
-			...config.externals,
-			"@mapbox/node-pre-gyp": "commonjs @mapbox/node-pre-gyp",
-		};
-		return config;
-	},
 };
 
 export default nextConfig;
