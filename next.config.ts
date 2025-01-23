240e373d653f778b8e85a6a1c99640528b6635dc
Anunay Argha <anunayargha@gmail.com>
2025-01-23 21:08:57 +0600

    build authentication

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	webpack: (config) => {
+		config.externals = {
+			...config.externals,
+			"@mapbox/node-pre-gyp": "commonjs @mapbox/node-pre-gyp",
+		};
+		return config;
+	},
 };
 
 export default nextConfig;
